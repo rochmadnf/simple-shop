e076814ee6bc7fab6cddc83258bf8c8e7f1ad207
rochmadnf <rochmadnf@gmail.com>
2022-05-11 18:48:02 +0800

    make main section and footer section

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -19,7 +19,11 @@
     {{-- component header --}}
     <header class="flex bg-white shadow-md shadow-slate-100 py-3 px-5 justify-between items-center">
         {{-- left --}}
-        <div class="logo">{{ env('APP_NAME') }}</div>
+        <div class="logo">
+            <a href="/">
+                {{ env('APP_NAME') }}
+            </a>
+        </div>
 
         {{-- middle --}}
         <div class="hidden md:flex rounded-full justify-around items-center pl-2 pr-4 border-2 border-gray-100">
@@ -40,6 +44,33 @@
             </div>
         </div>
     </header>
+
+    <main class="mt-2 py-10 px-0">
+        <div class="heading">
+            <h1 class="flex justify-center items-center text-3xl text-gray-700 font-semibold">Semua Produk</h1>
+        </div>
+
+        <section class="products">
+            <div class="card">
+                <div class="card-img">
+                    <img src="https://via.placeholder.com/300" alt="">
+                </div>
+                <div class="card-body">
+                    <div class="card-title">Brightening Body Serum</div>
+                    <div class="card-price">Rp50000</div>
+                    <div class="card-buttons">
+                        <button class="btn btn-outline btn-outline-primary">Beli Langsung</button>
+                        <button class="btn btn-primary">+ Keranjang</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer>
+        Test Case &copy; 2022 by Rochmad Nurul Fahmi
+    </footer>
 </body>
 
 </html>
